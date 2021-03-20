a97c4b3e981168832130b11ff2aeb0fa08bf621e
deepak bhatt <deepakbhattmits@gmail.com>
2021-03-20 11:50:53 +0530

    refactor

--- src/FromPage.jsx
+++ src/FromPage.jsx
@@ -2,6 +2,7 @@ const FormPage = ( {handleChange,handleSubmit, disabled} ) => {
     return (
       <>
         <form className="ui form" onSubmit={ handleSubmit }>
+    <div class="two fields">
           <div className="field">
             <label>Movie Name</label>
             <input type="text" name="movie" placeholder="Movie Name" onChange={ handleChange } />
@@ -9,6 +10,7 @@ const FormPage = ( {handleChange,handleSubmit, disabled} ) => {
           <div className="field">
             <label>Location</label>
             <input type="text" name="location" placeholder="Theater Name"  onChange={ handleChange }/>
+          </div>
           </div>
           <button className="ui button" type="submit" disabled={disabled}>Submit</button>
         </form>
